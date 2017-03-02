e4b08347be04f260c420cd39dc43e276810c5e5d
Sylvie <sylvie.veyres@gmail.com>
2017-03-02 15:46:16 +0100

    click sur tache ne marche pas

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-  $(function(){
-    $('.timer').startTimer();
-  });
-  })
+$(document).ready(function() {
+
+            $(".ajouter").click(function() {
+                var text = $('input').val();
+                $(".board").append('<li class="tache">' + text + '<span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span></li>')
+            })
+
+            $(".glyphicon.glyphicon-remove-sign").click(function(){
+                var maTache= $(this).parent();
+                console.log(maTache);
+                $(".board2").append(maTache);
+            })
+
+
+
+
+
+
+
+            // $(function() {
+            //             $('.timer').startTimer({
+            //                     onComplete: function() {
+            //
+            //
+            //             })
+})
